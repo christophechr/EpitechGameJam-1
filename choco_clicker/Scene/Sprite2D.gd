a29934b708bafb39e3511cc1ca76c3958795c81c
florian <florian.azema@epitech.eu>
2023-12-02 06:50:28 +0100

    erika add button

--- choco_clicker/Scene/Sprite2D.gd
+++ choco_clicker/Scene/Sprite2D.gd
@@ -1,5 +1,7 @@
-extends Area2D
+extends Sprite2D
 
+var normal_size = Vector2(1, 1)
+var smaller_size = Vector2(0.9, 0.9)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +11,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_choco_button_up():
+	scale = normal_size
+
+func _on_choco_button_down():
+	scale = smaller_size
